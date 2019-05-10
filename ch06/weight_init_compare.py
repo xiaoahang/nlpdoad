aa2e8c2223b99eecca34e35d2c440afa32d05d9f
hannah <hannah.zz@qq.com>
2019-05-10 17:24:22 +0800

    Xaiver初始值 和He初始值对比

--- ch06/weight_init_compare.py
+++ ch06/weight_init_compare.py
@@ -1,4 +1,12 @@
 # coding: utf-8
+#  Copyright (c)
+#  projectName: nlpdoad        # filename : weight_init_compare.py
+#  Author: weihangzhang         # email: hannah.zz@qq.com
+#  createDate : 2019/5/10 下午5:24
+#  lastModified: 2019/5/9 下午11:04
+#  desc:
+
+
 import os
 import sys
 
@@ -10,14 +18,12 @@ from common.util import smooth_curve
 from common.multi_layer_net import MultiLayerNet
 from common.optimizer import SGD
 
-
 # 0:读入MNIST数据==========
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
 
 train_size = x_train.shape[0]
 batch_size = 128
 max_iterations = 2000
-
 
 # 1:进行实验的设置==========
 weight_init_types = {'std=0.01': 0.01, 'Xavier': 'sigmoid', 'He': 'relu'}
@@ -30,28 +36,26 @@ for key, weight_type in weight_init_types.items():
                                   output_size=10, weight_init_std=weight_type)
     train_loss[key] = []
 
-
 # 2:开始训练==========
 for i in range(max_iterations):
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
-    
+
     for key in weight_init_types.keys():
         grads = networks[key].gradient(x_batch, t_batch)
         optimizer.update(networks[key].params, grads)
-    
+
         loss = networks[key].loss(x_batch, t_batch)
         train_loss[key].append(loss)
-    
+
     if i % 100 == 0:
         print("===========" + "iteration:" + str(i) + "===========")
         for key in weight_init_types.keys():
             loss = networks[key].loss(x_batch, t_batch)
             print(key + ":" + str(loss))
 
-
-# 3.绘制图形==========
+# 3.绘制图形========== 横轴是迭代学习的次数iterations，纵轴是损失函数的值loss
 markers = {'std=0.01': 'o', 'Xavier': 's', 'He': 'D'}
 x = np.arange(max_iterations)
 for key in weight_init_types.keys():
@@ -61,3 +65,7 @@ plt.ylabel("loss")
 plt.ylim(0, 2.5)
 plt.legend()
 plt.show()
+
+# 在这个实验中，神经网络有5层，每层有100哥神经元，激活函数使用的是ReLu。
+# 在代码绘制的图中可以看出，std=0.01时，完全无法学习（是因为在正向传播中的数值很小，因此逆向传播时求出的梯度也很小（集中在0附近的数据），权重几乎得不到更新）
+# 相反，当权重初始值为Xavier初始值和He初始值时，学习进行的很顺利（He初始值的学习进度更快些）
